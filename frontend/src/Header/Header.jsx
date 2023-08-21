108094fc8b2bdc4661e4fc8472f6de3b1caa41a9
m4tiolli <gabrielmatiollif@gmail.com>
2023-08-20 23:29:26 -0300

    mobile doq tem na landing page

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -1,14 +1,34 @@
 import Logo from "../assets/Logo";
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { FaUserAlt } from "react-icons/fa"
 
 export default function Header() {
+
+    const [isResponsive, setResponsive] = useState()
+
+    useEffect(() => {
+        const handleResize = () => {
+            setResponsive(window.innerWidth < 700);
+        };
+
+        handleResize(); // Set initial state
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
-        <div className="flex justify-between items-center absolute top-0 w-11/12">
+        <div className="flex justify-between items-center absolute top-0 w-11/12 pt-2">
             <Logo />
-            <div className="w-1/4 flex justify-evenly h-12">
-                <Link to={"/login"} className="flex items-center justify-center bg-[#F1E0FF] w-2/5 h-full rounded-xl shadow-md text-cinza font-bold text-xl hover:opacity-70 active:translate-y-1 active:shadow-lg">Entrar</Link>
-                <Link to={"/criar-conta"} className="flex items-center justify-center bg-[#F4BA84] w-2/5 h-full rounded-xl shadow-md text-cinza font-bold text-xl hover:opacity-70 active:translate-y-1 active:shadow-lg">Criar Conta</Link>
+            <div className="flex w-3/4 justify-between h-8">
+                <Link to={"/login"} className="flex items-center justify-center bg-[#F1E0FF] h-full rounded-xl shadow-md text-cinza fod text-md hover:opacity-70 active:translate-y-1 active:shadow-xs px-2">{!isResponsive ? <button>Entrar</button> : <FaUserAlt />}</Link>
+                <Link to={"/criar-conta"} className="flex items-center justify-center bg-[#F4BA84] w-fit h-full rounded-lg shadow-md text-cinza font-bold text-md hover:opacity-70 active:translate-y-1 active:shadow-xs px-2"><button>Criar Conta</button></Link>
             </div>
         </div>
     )
+
 }
