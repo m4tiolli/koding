9bb865e719955ea51fdbc1e95bc9575a521c3079
marquinhosjpg <mrclahdf23@gmail.com>
2023-08-27 16:10:25 -0300

    ajustes

--- frontend/src/Screens/Aprender/Aprender.jsx
+++ frontend/src/Screens/Aprender/Aprender.jsx
@@ -6,31 +6,31 @@ function Aprender(){
             <h1 className="text-6xl text-gray-600/70 font-semibold">O que vamos aprender?</h1>
             <container className="flex gap-16 mt-20">
 
-                <div className="h-72 w-60 bg-black rounded-3xl"style={{ background: "linear-gradient(108deg, #E87331 0%, #E88D59 100%)" }}>
-                    <div className="flex flex-col text-white mt-32 ml-4">
+                <div className="h-72 w-60 bg-black rounded-3xl"style={{ background: "linear-gradient(10deg,#E88D59 0%, #E87331 100%)" }}>
+                    <div className="flex flex-col text-white mt-32 ml-4 gap-y-3">
                         <span className="font-bold text-2xl">HTML</span>
-                        <span className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        <span className="w-48">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
                     </div>
                 </div>
 
-                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(108deg, #1566C4 0%, #438ADD 100%)" }}>
-                    <div className="flex flex-col text-white mt-32 ml-4">
+                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(10deg, #438ADD 0%, #1566C4 100%)" }}>
+                    <div className="flex flex-col text-white mt-32 ml-4 gap-y-3">
                         <span className="font-bold text-2xl">CSS</span>
-                        <span className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        <span className="w-48">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
                     </div>
                 </div>
 
-                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(108deg, #F2D01F 0%, #E3BA4F 100%)" }}>
-                    <div className="flex flex-col text-white mt-32 ml-4">
+                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(10deg, #E3BA4F 0%, #F2D01F 100%)" }}>
+                    <div className="flex flex-col text-white mt-32 ml-4 gap-y-3">
                         <span className="font-bold text-2xl">JavaScript</span>
-                        <span className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        <span className="w-48">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
                     </div>
                 </div>
 
-                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(108deg, #485BBB 0%, #707EC6 100%)" }}>
-                    <div className="flex flex-col text-white mt-32 ml-4">
+                <div className="h-72 w-60 bg-black rounded-3xl" style={{ background: "linear-gradient(10deg, #707EC6 0%, #485BBB 100%)" }}>
+                    <div className="flex flex-col text-white mt-32 ml-4 gap-y-3">
                         <span className="font-bold text-2xl">PHP</span>
-                        <span className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        <span className="w-48">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
                     </div>
                 </div>
             </container>
