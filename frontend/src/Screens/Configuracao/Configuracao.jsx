53409e1a8d90cb06edcf7d6d4ce7129e5122936e
marquinhosjpg <mrclahdf23@gmail.com>
2023-10-04 17:03:40 -0300

    config responsa agora

--- frontend/src/Screens/Configuracao/Configuracao.jsx
+++ frontend/src/Screens/Configuracao/Configuracao.jsx
@@ -17,7 +17,6 @@ import {
 
 // import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton,} from '@chakra-ui/react';
 
-import SideBar from "../../Components/SideBar/SideBar";
 import Menu from "../../Components/Menu/Menu";
 
 function Configuracao() {
@@ -35,14 +34,14 @@ function Configuracao() {
   return (
     <ChakraProvider>
       <div
-        className="flex h-screen w-full"
+        className="flex h-full w-full"
         style={{
           background: "linear-gradient(108deg, #E5C6FF 0%, #E4EBFF 100%)",
         }}
       >
         <Menu />
 
-        <main className="w-full ml-60 mr-2 mt-[10.8rem] overflow-hidden">
+        <main className="w-full ml-60 mr-2 mt-[5.8rem] overflow-hidden">
           <div className="mb-10">
             <span className="text-3xl font-semibold">Configurações</span>
           </div>
@@ -52,7 +51,7 @@ function Configuracao() {
           <container className="w-full h-full flex flex-col gap-y-10 ml-16">
             {/* Enviar Foto */}
 
-            <form action="" method="" className="flex gap-x-96">
+            <form action="" method="" className="flex flex-col lg:flex-row gap-x-32 lg:gap-x-64">
               <div className="flex items-center gap-6">
                 <span className="text-lg">Personalizar Avatar</span>
                 <label
@@ -68,7 +67,7 @@ function Configuracao() {
 
               <div>
                 <button
-                  className="bg-green-500 w-auto h-10 p-3 flex items-center rounded-xl shadow-lg text-white"
+                  className="bg-green-500 w-auto h-10 p-3 mt-5 lg:mt-0 flex items-center rounded-xl shadow-lg text-white"
                   type="submit"
                 >
                   Salvar Alterações
@@ -83,14 +82,14 @@ function Configuracao() {
               method="post"
               className="flex flex-col gap-y-5 text-lg"
             >
-              <div className="w-2/5 flex items-center">
+              <div className="w-2/5 lg:w-[440px] flex flex-col lg:flex-row items-center">
                 <div className="w-full">
                   <span>Nome</span>
                 </div>
 
                 <label className="">
                   <input
-                    className="p-2 w-42 rounded-xl border-solid border-black outline-none shadow-lg"
+                    className="ml-8 p-2 w-42 rounded-xl border-solid border-black outline-none shadow-lg"
                     style={{ background: "#efefef" }}
                     name="name"
                     type="text"
@@ -98,14 +97,14 @@ function Configuracao() {
                 </label>
               </div>
 
-              <div className="w-2/5 flex items-center">
+              <div className="w-2/5 lg:w-[440px] flex flex-col lg:flex-row items-center">
                 <div className="w-full">
                   <span>Username</span>
                 </div>
 
                 <label>
                   <input
-                    className="p-2 rounded-xl border-solid border-black outline-none shadow-lg"
+                    className="ml-8 p-2 rounded-xl border-solid border-black outline-none shadow-lg"
                     style={{ background: "#efefef" }}
                     name="username"
                     type="text"
@@ -113,14 +112,14 @@ function Configuracao() {
                 </label>
               </div>
 
-              <div className="w-2/5 flex items-center">
+              <div className="w-2/5 lg:w-[440px] flex flex-col lg:flex-row items-center">
                 <div className="w-full">
                   <span>E-mail</span>
                 </div>
 
                 <label>
                   <input
-                    className="p-2 rounded-xl border-solid border-black outline-none shadow-lg"
+                    className="ml-8 p-2 rounded-xl border-solid border-black outline-none shadow-lg"
                     style={{ background: "#efefef" }}
                     name="email"
                     type="email"
@@ -128,14 +127,14 @@ function Configuracao() {
                 </label>
               </div>
 
-              <div className="w-2/5 flex items-center">
+              <div className="w-2/5 lg:w-[440px] flex flex-col lg:flex-row items-center">
                 <div className="w-full">
                   <span>Senha</span>
                 </div>
 
                 <label>
                   <input
-                    className="p-2 rounded-xl border-solid border-black outline-none shadow-lg"
+                    className="ml-8 p-2 rounded-xl border-solid border-black outline-none shadow-lg"
                     style={{ background: "#efefef" }}
                     name="password"
                     type="password"
@@ -143,14 +142,14 @@ function Configuracao() {
                 </label>
               </div>
 
-              <div className="w-2/5 flex items-center">
+              <div className="w-2/5 lg:w-[440px] flex flex-col lg:flex-row items-center">
                 <div className="w-full">
                   <span>Confirmar senha</span>
                 </div>
 
                 <label>
                   <input
-                    className="p-2 rounded-xl border-solid border-black outline-none shadow-lg"
+                    className="ml-8 p-2 rounded-xl border-solid border-black outline-none shadow-lg"
                     style={{ background: "#efefef" }}
                     name="password"
                     type="password"
@@ -162,13 +161,13 @@ function Configuracao() {
             {/* Modos */}
 
             <div className="flex flex-col gap-y-5 text-lg">
-              <div className="flex">
+              <div className="flex flex-col">
                 <div className="w-52">
                   <span>Modo Escuro</span>
                 </div>
 
                 <select
-                  className="p-2 w-42 rounded-xl border-solid border-black outline-none shadow-lg cursor-pointer"
+                  className="p-2 w-[250px] mt-2 rounded-xl border-solid border-black outline-none shadow-lg cursor-pointer"
                   style={{ background: "#efefef" }}
                   name="darkMode"
                   id="darkMode"
@@ -179,13 +178,13 @@ function Configuracao() {
                 </select>
               </div>
 
-              <div className="flex">
+              <div className="flex flex-col">
                 <div className="w-52">
                   <span>Alto Contraste</span>
                 </div>
 
                 <select
-                  className="p-2 w-42 rounded-xl border-solid border-black outline-none shadow-lg cursor-pointer"
+                  className="p-2 w-[250px] mt-2 rounded-xl border-solid border-black outline-none shadow-lg cursor-pointer"
                   style={{ background: "#efefef" }}
                   name="contraste"
                   id="contraste"
