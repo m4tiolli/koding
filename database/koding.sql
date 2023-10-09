79e5c26cc4690a3fae6f60d8e18e1cfdceaac255
jubsga <112034686+jubsga@users.noreply.github.com>
2023-10-09 14:15:19 -0300

    db semeado

--- database/koding.sql
+++ database/koding.sql
@@ -85,21 +85,69 @@ CREATE TABLE IF NOT EXISTS JogosCrianca(
 	FOREIGN KEY (jogo) REFERENCES jogos (id)
 );
 
-INSERT INTO responsavel VALUES (DEFAULT, 'ju', '13142413', '7236294', 'ju@gmail.com', '321');
+INSERT INTO responsavel VALUES 
+(DEFAULT, 'ju', '13142413', '7236294', 'ju@gmail.com', '321'),
+(DEFAULT, 'matiolli', '13142413', '7236294', 'matias@gmail.com', '321'),
+(DEFAULT, 'marcos', '13142413', '7236294', 'marcos@gmail.com', '321');
 
-INSERT INTO crianca VALUES (DEFAULT, 'matias', 'marcao', 'koding@gmail.com', '123', 1);
+INSERT INTO crianca VALUES 
+(DEFAULT, 'matiolli', 'matias', 'matias@gmail.com', '123', 1),
+(DEFAULT, 'marcos', 'marcos', 'marcos@gmail.com', '123', 2),
+(DEFAULT, 'ju', 'ju', 'ju@gmail.com', '123', 3);
 
-INSERT INTO pontuacoes VALUES (DEFAULT, 1, 100, '2023-10-08');
+INSERT INTO pontuacoes VALUES (DEFAULT, 1, 100, '2023-10-07');
+INSERT INTO pontuacoes VALUES (DEFAULT, 2, 200, '2023-10-08');
+INSERT INTO pontuacoes VALUES (DEFAULT, 3, 300, '2023-10-09');
 
-INSERT INTO linguagens VALUES (DEFAULT, 'HTML');
+INSERT INTO linguagens VALUES
+(DEFAULT, 'HTML'),
+(DEFAULT, 'CSS'),
+(DEFAULT, 'JavaScript'),
+(DEFAULT, 'PHP');
 
-INSERT INTO tags VALUES (DEFAULT, 'HTML');
+INSERT INTO tags VALUES 
+(DEFAULT, 'HTML'),
+(DEFAULT, 'CSS'),
+(DEFAULT, 'JavaScript'),
+(DEFAULT, 'PHP'),
+(DEFAULT, 'Estrutura'),
+(DEFAULT, 'Flexbox'),
+(DEFAULT, 'Input'),
+(DEFAULT, 'Introdução');
 
-INSERT INTO capitulos VALUES (DEFAULT, 'Introdução ao HTML', 1);
+INSERT INTO capitulos VALUES 
+(DEFAULT, 'Introdução ao HTML', 1),
+(DEFAULT, 'Introdução ao CSS', 2),
+(DEFAULT, 'Introdução ao JavaScript', 3),
+(DEFAULT, 'Introdução ao PHP', 4);
 
-INSERT INTO aulas VALUES (DEFAULT, 'Aula 00', 'Aula referente a HTML e suas tags', 'longlonglonglonglonglongloblob', 'html.png', 1);
+INSERT INTO aulas VALUES 
+(DEFAULT, 'Aula 00', 'Sintaxe do HTML', 'longlonglonglonglonglongloblob', 
+'html.png', 1),
+(DEFAULT, 'Aula 01', 'Primeira página em HTML', 'longlonglonglonglonglongloblob', 
+'html.png', 1),
+(DEFAULT, 'Aula 00', 'Sintaxe do CSS', 'longlonglonglonglonglongloblob', 
+'html.png', 2),
+(DEFAULT, 'Aula 00', 'Primeira página em CSS', 'longlonglonglonglonglongloblob', 
+'html.png', 2),
+(DEFAULT, 'Aula 00', 'Sintaxe do JavaScript', 'longlonglonglonglonglongloblob', 
+'html.png', 3),
+(DEFAULT, 'Aula 00', 'Primeira página em JavaScript', 'longlonglonglonglonglongloblob', 
+'html.png', 3),
+(DEFAULT, 'Aula 00', 'Sintaxe do PHP', 'longlonglonglonglonglongloblob', 
+'html.png', 4),
+(DEFAULT, 'Aula 00', 'Primeira página em PHP', 'longlonglonglonglonglongloblob', 
+'html.png', 4);
 
-INSERT INTO TagsAula VALUES (1, 1);
+INSERT INTO TagsAula VALUES 
+(1, 1),
+(1, 2),
+(2, 3),
+(2, 4),
+(3, 5),
+(3, 6),
+(4, 7),
+(4, 8);
 
 SELECT crianca.username, crianca.responsavel, pontuacoes.pontuacao AS Pontuação
 FROM crianca INNER JOIN pontuacoes ON pontuacoes.crianca = crianca.id;
