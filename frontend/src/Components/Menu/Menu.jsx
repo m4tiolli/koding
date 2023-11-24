14dbf1474af38852f4d06fad5b484620b5a2cc50
marquinhosjpg <mrclahdf23@gmail.com>
2023-11-24 20:55:25 -0300

    ajustes não ajustados

--- frontend/src/Components/Menu/Menu.jsx
+++ frontend/src/Components/Menu/Menu.jsx
@@ -453,7 +453,7 @@ export default function Menu({ screen }) {
         <ModalOverlay />
         <ModalContent
           minWidth={"45vw"}
-          h={"45vh"}
+          h={"75h"}
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
