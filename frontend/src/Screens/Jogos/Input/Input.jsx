1f76d4790db334480867d6004529a9f6b4ba79f0
m4tiolli <gabrielmatiollif@gmail.com>
2023-11-24 23:20:32 -0300

    input finalizado!

--- frontend/src/Screens/Jogos/Input/Input.jsx
+++ frontend/src/Screens/Jogos/Input/Input.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineClose } from "react-icons/ai";
-
+import { ToastContainer, toast } from "react-toastify";
 import "../Input/Input.css";
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import { Modal } from "@chakra-ui/react";
+import { ModalOverlay } from "@chakra-ui/react";
+import { ModalContent } from "@chakra-ui/react";
+import { FaRegCheckCircle } from "react-icons/fa";
 
 function Input() {
   const fases = [
@@ -38,8 +43,7 @@ function Input() {
       setResposta("");
     } else {
       enviarPontuacao();
-      alert("Você completou todas as fases!");
-      navigate("/desafios");
+      setTimeout(() => onOpen(), 2000);
     }
   };
 
@@ -62,6 +66,7 @@ function Input() {
     });
     return fraseRenderizada;
   };
+  var local = parseInt(localStorage.pontuacao);
 
   const verificarResposta = () => {
     const fase = fases[faseAtual];
@@ -69,8 +74,16 @@ function Input() {
       fase.tipo === "alternativa" &&
       respostaEscolhida.join("") === fase.respostaCorreta
     ) {
-      alert("Você acertou!");
-      var local = parseInt(localStorage.pontuacao);
+      toast.success("Você acertou!", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
       local = local + 100;
       localStorage.setItem("pontuacao", local);
       avancarFase();
@@ -78,109 +91,169 @@ function Input() {
       fase.tipo === "escrita" &&
       respostaEscrita.toLowerCase() === fase.respostaCorreta.toLowerCase()
     ) {
-      alert("Você acertou!");
-      var local = parseInt(localStorage.pontuacao);
+      toast.success("Você acertou!", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+
       local = local + 100;
       localStorage.setItem("pontuacao", local);
       avancarFase();
     } else {
-      alert("Você errou!");
+      toast.error("Você errou!", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
       avancarFase();
     }
   };
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  function Fechar() {
+    onClose();
+    navigate("/desafios");
+  }
+
   const enviarPontuacao = () => {
-    const crianca = localStorage.getItem("id");
-    const pontuacao = localStorage.getItem("pontuacao");
-    const data = new Date();
-    const body = { crianca, pontuacao, data };
+    const user = JSON.parse(localStorage.getItem("user"));
+    const crianca = user.id;
+    const pontuacao = parseInt(localStorage.getItem("pontuacao"));
+    const body = { crianca, pontuacao };
+    console.log(body);
     fetch(`https://tcckoding.azurewebsites.net/crianca/pontuacao`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
     })
       .then(() => {
-        alert("Pontuação enviada com sucesso");
         localStorage.removeItem("pontuacao");
       })
       .catch((error) => {
         console.log(error);
-        alert("Erro ao enviar a pontuação");
       });
   };
 
   const navigate = useNavigate();
 
   return (
-    <div
-      className="flex h-screen w-full"
-      style={{
-        background: "linear-gradient(108deg, #E5C6FF 0%, #E4EBFF 100%)",
-      }}
-    >
-      <main className="w-full overflow-hidden dark:bg-darkcinzaclaro min-h-screen dark:text-white">
-        <div className="flex items-center mt-10 gap-x-48">
-          <AiOutlineClose
-            onClick={() => navigate(-1)}
-            className="flex ml-8 text-3xl cursor-pointer text-gray-400 dark:text-white"
-          />
-          
-          <div className="w-7/12 h-5 bg-gray-300 rounded-lg"></div>
-        </div>
+    <ChakraProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        limit={3}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <div
+        className="flex h-screen w-full"
+        style={{
+          background: "linear-gradient(108deg, #E5C6FF 0%, #E4EBFF 100%)",
+        }}
+      >
+        <main className="w-full overflow-hidden dark:bg-darkcinzaclaro min-h-screen dark:text-white">
+          <div className="flex items-center mt-10 gap-x-48">
+            <AiOutlineClose
+              onClick={() => navigate(-1)}
+              className="flex ml-8 text-3xl cursor-pointer text-gray-400 dark:text-white"
+            />
 
-
-        <div className="flex flex-col justify-center items-center mt-20">
-          <h1 className="flex ml-10 font-bold text-gray-700 text-2xl dark:text-white">
-            {titulo}
-          </h1>
-
-          <div className="flex flex-col items-center justify-center mt-10">
-            {resposta && <p>{resposta}</p>}
-            {faseAtual < fases.length && (
-              <div className="text-xl text-gray-700 dark:text-white">
-                <h2>{renderFrase()}</h2>
-                {fases[faseAtual].tipo === "alternativa" ? (
-                  <div className="flex flex-col mt-10 w-auto h-16">
-                    <div className="flex gap-x-10">
-                      {fases[faseAtual].opcoes.map((opcao, index) => (
-                        <button
-                          key={index}
-                          onClick={() => handleWordClick(opcao)}
-                          className={
-                            respostaEscolhida.includes(opcao)
-                              ? "selected"
-                              : "bg-purple-700 p-2 div rounded-xl text-white"
-                          }
-                        >
-                          {opcao}
-                        </button>
-                      ))}
-                    </div>
-                  </div>
-                ) : (
-                  <div className="flex justify-center items-center">
-                    <input
-                      className="p-2 rounded-xl mt-16 mb-10 border-none outline-none w-96"
-                      type="text"
-                      value={respostaEscrita}
-                      onChange={(e) => setRespostaEscrita(e.target.value)}
-                    />
-                  </div>
-                )}
-                <div className="flex items-center justify-center mt-24">
-                  <button
-                    className="flex rounded-md w-32 text-base bg-green-300 dark:text-gray-800 font-semibold"
-                    onClick={verificarResposta}
-                  >
-                    Verificar Resposta
-                  </button>
-                </div>
-              </div>
-            )}
+            <div className="w-7/12 h-5 bg-gray-300 rounded-lg"></div>
           </div>
-        </div>
-      </main>
-    </div>
+
+          <div className="flex flex-col justify-center items-center mt-20">
+            <h1 className="flex ml-10 font-bold text-gray-700 text-2xl dark:text-white">
+              {titulo}
+            </h1>
+
+            <div className="flex flex-col items-center justify-center mt-10">
+              {resposta && <p>{resposta}</p>}
+              {faseAtual < fases.length && (
+                <div className="text-xl text-gray-700 dark:text-white">
+                  <h2>{renderFrase()}</h2>
+                  {fases[faseAtual].tipo === "alternativa" ? (
+                    <div className="flex flex-col mt-10 w-auto h-16">
+                      <div className="flex gap-x-10">
+                        {fases[faseAtual].opcoes.map((opcao, index) => (
+                          <button
+                            key={index}
+                            onClick={() => handleWordClick(opcao)}
+                            className={
+                              respostaEscolhida.includes(opcao)
+                                ? "selected"
+                                : "bg-purple-700 p-2 div rounded-xl text-white"
+                            }
+                          >
+                            {opcao}
+                          </button>
+                        ))}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex justify-center items-center">
+                      <input
+                        className="p-2 rounded-xl mt-16 mb-10 border-none outline-none w-96 dark:text-cinza"
+                        type="text"
+                        value={respostaEscrita}
+                        onChange={(e) => setRespostaEscrita(e.target.value)}
+                      />
+                    </div>
+                  )}
+                  <div className="flex items-center justify-center mt-24">
+                    <button
+                      className="flex rounded-md w-32 text-base bg-green-300 dark:text-gray-800 font-semibold"
+                      onClick={verificarResposta}
+                    >
+                      Verificar Resposta
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </main>
+        <Modal isOpen={isOpen} onClose={onClose} motionPreset="scale">
+          <ModalOverlay />
+          <ModalContent
+            borderRadius={"20vw"}
+            w={"40vw"}
+            h={"fit-content"}
+            top={"20vh"}
+          >
+            <div className="w-full h-full flex flex-col justify-center items-center bg-green-700 rounded-2xl gap-6 p-10">
+              <FaRegCheckCircle color="rgb(134, 239, 172)" size={50} />
+              <p className="text-white text-xl font-semibold">
+                Você completou todas as fases!
+              </p>
+              <button
+                onClick={Fechar}
+                className="text-green-700 bg-white w-2/5 h-10 rounded-xl text-xl font-semibold hover:opacity-70"
+              >
+                Sair
+              </button>
+            </div>
+          </ModalContent>
+        </Modal>
+      </div>
+    </ChakraProvider>
   );
 }
 
