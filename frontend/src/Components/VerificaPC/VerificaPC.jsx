88ba2a21720f4141e50bdee82089ef9bb9ff4d59
marquinhosjpg <mrclahdf23@gmail.com>
2023-12-09 09:40:30 -0300

    errei

--- frontend/src/Components/VerificaPC/VerificaPC.jsx
+++ frontend/src/Components/VerificaPC/VerificaPC.jsx
@@ -14,21 +14,26 @@ function VerificaPC() {
     };
   }, []);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const Sair = () => {
     localStorage.removeItem("user");
     setTimeout(() => {
-      avigate("/")
+      navigate("/");
     }, 1000);
-  }
+  };
   return pc ? (
     <div className="fixed w-screen h-screen p-8 z-50 bg-darkcinza text-white flex flex-col items-center justify-center gap-4">
       <MdDevices className="text-[10em]" />
       <p className="text-xl text-center">
         Para uma melhor experiência acesse nossa plataforma por um computador.
       </p>
-      <button onClick={Sair} className="bg-white  text-darkcinza w-3/5 py-6 my-4 rounded-xl text-2xl">Sair</button>
+      <button
+        onClick={Sair}
+        className="bg-white  text-darkcinza w-3/5 py-6 my-4 rounded-xl text-2xl"
+      >
+        Sair
+      </button>
     </div>
   ) : (
     ""
